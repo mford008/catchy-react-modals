2eda87fc22a43de4547ee437a3c36f248fe3f3d8
Maddy Ford <mrhf008@gmail.com>
2018-08-17 14:02:18 -0700

    moved dist files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
-import React, {Component} from 'react';
 import Button from './Button/Button.js';
 import Modal from './Modal/Modal.js';
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
-  }
-}
+
 export {
   Button,
   Modal
-}
+};
